8b1270238935c951ec59c41dafd62007dfaceecb
Esther Halls <esthernhalls@gmail.com>
2023-01-25 17:20:15 -0700

    Initial Commit

---
+++ DogAPI/DogAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogAPIApp.swift
+//  DogAPI
+//
+//  Created by Esther on 1/25/23.
+//
+
+import SwiftUI
+
+@main
+struct DogAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
