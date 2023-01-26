9f555f851478f784f181f70285b3afd564de89ab
Esther Halls <esthernhalls@gmail.com>
2023-01-25 23:16:49 -0700

    altered URL endpoint page to hold iterative cases for endpoints

--- DogAPI/ContentView.swift
+++ DogAPI/ContentView.swift
@@ -36,7 +36,7 @@ struct ContentView: View {
 //            Image(systemName: "globe")
 //                .imageScale(.large)
 //                .foregroundColor(.accentColor)
-//            Text("Hello, world!")
+//            Text("Hello, world!")
 //            Button("Previous Image") {
 //
 //            }
